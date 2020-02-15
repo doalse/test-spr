b375cade5e218120e2cb99e56f259003e25f499c
Alex <doaslekeeper@gmail.com>
2020-02-15 21:54:18 +0200

    fixed 10 point

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,25 @@
 "use strict";
 
 $(function() {
-    $(".nav-bottom__search-ico").on("click", function() {
-        $(".mobile-search-bar").slideToggle("fast");
-        $(".mobile-search-bar__input")
-            .val("")
-            .focus();
-    });
-
     $(".nav-bottom__btn").on("click", function() {
         $(".nav-mobile-menu").toggleClass("nav-mobile-menu_show");
+    });
+
+    $(window).on("resize load", function() {
+        if ($(window).width() < 1280) {
+            $(".nav-bottom__submit").attr("data-active", "false");
+        } else {
+            $(".nav-bottom__submit").attr("data-active", "true");
+        }
+    });
+
+    $(".nav-bottom__submit").on("click", function(e) {
+        if ($(this).attr("data-active") == "false") {
+            e.preventDefault();
+            $(".mobile-search-bar").slideToggle("fast");
+            $(".mobile-search-bar__input")
+                .val("")
+                .focus();
+        }
     });
 });
