ec4ba4ced7766691a7123a432019600b165aeb39
Alex <doaslekeeper@gmail.com>
2020-02-15 22:28:33 +0200

    added js prefixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 "use strict";
 
 $(function() {
-    $(".nav-bottom__btn").on("click", function() {
+    $(".js-nav-bottom__btn").on("click", function() {
         $(".nav-mobile-menu").toggleClass("nav-mobile-menu_show");
     });
 
     $(window).on("resize load", function() {
         if ($(window).width() < 1280) {
-            $(".nav-bottom__submit").attr("data-active", "false");
+            $(".js-nav-bottom__submit").attr("data-active", "false");
         } else {
-            $(".nav-bottom__submit").attr("data-active", "true");
+            $(".js-nav-bottom__submit").attr("data-active", "true");
         }
     });
 
-    $(".nav-bottom__submit").on("click", function(e) {
+    $(".js-nav-bottom__submit").on("click", function(e) {
         if ($(this).attr("data-active") == "false") {
             e.preventDefault();
             $(".mobile-search-bar").slideToggle("fast");
