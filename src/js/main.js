18b801274846a3b4450d965c026e606cda542206
Alex <doaslekeeper@gmail.com>
2020-02-16 18:08:33 +0200

    added autoplay to slider

--- src/js/main.js
+++ src/js/main.js
@@ -29,6 +29,8 @@ $(function() {
         infinite: true,
         slidesToShow: 1,
         dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
         customPaging: function(slider, i) {
             return `<a class="slick-dot__element"></a>`;
         }
