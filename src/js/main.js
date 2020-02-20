18322139f2db18ef71804a8cfd9ea9a89c204485
Alex <doaslekeeper@gmail.com>
2020-02-20 23:29:38 +0200

    fixed mobile menu & search bar

--- src/js/main.js
+++ src/js/main.js
@@ -2,24 +2,11 @@
 
 $(function() {
     $(".js-nav-bottom__btn").on("click", function() {
-        $(".nav-mobile-menu").toggleClass("nav-mobile-menu_show");
+        $(".nav-bottom-menu").toggleClass("nav-bottom-menu_show");
     });
 
-    $(window).on("resize load", function() {
-        if ($(window).width() < 1280) {
-            $(".js-nav-bottom__submit").attr("data-active", "false");
-        } else {
-            $(".js-nav-bottom__submit").attr("data-active", "true");
-        }
-    });
-
-    $(".js-nav-bottom__submit").on("click", function(e) {
-        if ($(this).attr("data-active") == "false") {
-            e.preventDefault();
-            $(".mobile-search-bar").slideToggle("fast");
-            $(".mobile-search-bar__input")
-                .val("")
-                .focus();
-        }
+    $(".nav-bottom__search-ico").on("click", function() {
+        $(".nav-bottom__search-cart").slideToggle("fast");
+        $(".nav-bottom__input").val("").focus();
     });
 });
